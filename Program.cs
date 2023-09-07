cb1c5f61d6b30c3ee77989303a7d33635410d015
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 01:39:57 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("a = {0}",a);
+        Console.WriteLine("b = {0}",b);
+        int s = a + b;
+        Console.WriteLine("{0} + {1} = {2}", a,b,s);
+    }
+}
