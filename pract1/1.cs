de6b8df54f58e06c7fb6d01a071eaebe107b9d15
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 10:32:33 +0400

    Update 1.cs

--- pract1/1.cs
+++ pract1/1.cs
@@ -1,12 +1,15 @@
-﻿public class Program
+using System;
+public class Program
 {
     public static void Main()
     {
+        Console.Write("a = ");
         int a = int.Parse(Console.ReadLine());
+        Console.Write("b = ");
         int b = int.Parse(Console.ReadLine());
-        Console.WriteLine("a = {0}",a);
-        Console.WriteLine("b = {0}",b);
+        //Console.WriteLine("a = {0}", a);
+        //Console.WriteLine("b = {0}", b);
         int s = a + b;
-        Console.WriteLine("{0} + {1} = {2}", a,b,s);
+        Console.WriteLine("{0} + {1} = {2}", a, b, s);
     }
 }
