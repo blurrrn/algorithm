0dda83b2605248065deb6577a16ddee1a0e153f4
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 10:52:14 +0400

    Create pract2-II-11.cs

---
+++ pract2-II-11.cs
@@ -0,0 +1,23 @@
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите число А = ");
+        int a = int.Parse(Console.ReadLine());
+        int x1 = a / 100;
+        int x2 = a % 100 / 10;
+        int x3 = a % 10;
+        Console.WriteLine("Цифры числа A: {0}, {1}, {2}",x1,x2,x3);
+        if (x1!=x2 & x2 != x3 & x1!=x3)
+        {
+            Console.WriteLine("Цифры числа A разные.");
+        }
+        else
+        {
+            Console.WriteLine("Цифры числа A НЕ разные.");
+        }
+
+
+    }
+}
