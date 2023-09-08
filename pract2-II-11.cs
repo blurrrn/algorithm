5715622aafdf33fab6e3eca92f6b89a18e77443e
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 11:04:47 +0400

    Update pract2-II-11.cs

--- pract2-II-11.cs
+++ pract2-II-11.cs
@@ -20,15 +20,8 @@ public class Program
             int x1 = a / 100;
             int x2 = a % 100 / 10;
             int x3 = a % 10;
-            Console.WriteLine("Цифры числа A: {0}, {1}, {2}", x1, x2, x3);
-            if (x1 != x2 & x2 != x3 & x1 != x3)
-            {
-                Console.WriteLine("Цифры числа A разные.");
-            }
-            else
-            {
-                Console.WriteLine("Цифры числа A НЕ разные.");
-            }
+            string s = (x1 != x2 & x2 != x3 & x1 != x3) ? "Разные" : "Не разные";
+            Console.WriteLine(s);
         }
     }
 }
