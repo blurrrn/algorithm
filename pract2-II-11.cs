020798e9b54b6f6eb5b7a38be1a43de28372728d
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 11:18:58 +0400

    Update pract2-II-11.cs

--- pract2-II-11.cs
+++ pract2-II-11.cs
@@ -5,13 +5,8 @@ public class Program
     {
         Console.Write("Введите число А = ");
         int a = int.Parse(Console.ReadLine());
-        int k = 0;
-        while (a > 0)
-        {
-            k += 1;
-            a /= 10;
-        }
-        if (k != 3)
+
+        if (a < 100 || a> 999)
         {
             Console.WriteLine("Данное число не является трёхзначным.");
         }
@@ -20,7 +15,7 @@ public class Program
             int x1 = a / 100;
             int x2 = a % 100 / 10;
             int x3 = a % 10;
-            string s = (x1 != x2 & x2 != x3 & x1 != x3) ? "Разные" : "Не разные";
+            string s = (x1 != x2 && x2 != x3 && x1 != x3) ? "Разные" : "Не разные";
             Console.WriteLine(s);
         }
     }
