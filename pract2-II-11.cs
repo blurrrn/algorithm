be5d1098fb3d0a7c5874da2d71b03df761d3741f
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-08 10:57:00 +0400

    Update pract2-II-11.cs

--- pract2-II-11.cs
+++ pract2-II-11.cs
@@ -5,19 +5,30 @@ public class Program
     {
         Console.Write("Введите число А = ");
         int a = int.Parse(Console.ReadLine());
-        int x1 = a / 100;
-        int x2 = a % 100 / 10;
-        int x3 = a % 10;
-        Console.WriteLine("Цифры числа A: {0}, {1}, {2}",x1,x2,x3);
-        if (x1!=x2 & x2 != x3 & x1!=x3)
+        int k = 0;
+        while (a > 0)
         {
-            Console.WriteLine("Цифры числа A разные.");
+            k += 1;
+            a /= 10;
+        }
+        if (k != 3)
+        {
+            Console.WriteLine("Данное число не является трёхзначным.");
         }
         else
         {
-            Console.WriteLine("Цифры числа A НЕ разные.");
+            int x1 = a / 100;
+            int x2 = a % 100 / 10;
+            int x3 = a % 10;
+            Console.WriteLine("Цифры числа A: {0}, {1}, {2}", x1, x2, x3);
+            if (x1 != x2 & x2 != x3 & x1 != x3)
+            {
+                Console.WriteLine("Цифры числа A разные.");
+            }
+            else
+            {
+                Console.WriteLine("Цифры числа A НЕ разные.");
+            }
         }
-
-
     }
 }
