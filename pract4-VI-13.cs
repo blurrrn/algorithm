7b0753de03fecbeea6e22db9dcf9cff580884d33
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-14 23:54:56 +0400

    Create pract4-VI-13.cs

---
+++ pract4-VI-13.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    static void Main()
+    {
+        int ans = 0, N;
+        Console.Write("Введите N: ");
+        N = int.Parse(Console.ReadLine());
+        while (N != 0)
+        {
+            ans = N;
+            N /= 10;
+        }
+        Console.WriteLine($"Старшая цифра: {ans}");
+    }
+}
