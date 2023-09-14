43c63c8d7e4d71b2f2fc1addb1b9f9eb3865de3a
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-14 23:49:38 +0400

    Create pract3-IV-11.cs

---
+++ pract3-IV-11.cs
@@ -0,0 +1,19 @@
+class Program
+{
+    static void Main()
+    {
+        int A, B;
+        Console.Write("Введите A: ");
+        A =int.Parse(Console.ReadLine());
+        Console.Write("Введите B: ");
+        B = int.Parse(Console.ReadLine());
+        while (A % 5 != 0)
+        {
+            A++;
+        }
+        for (int x = A; x <= B; x+=5)
+        {
+            Console.WriteLine("{0} ",x);
+        }
+    }
+}
