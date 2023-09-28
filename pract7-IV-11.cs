dfe8003b385d9386fc079427ba857af439c13203
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-09-29 01:32:13 +0400

    Create pract7-IV-11.cs

---
+++ pract7-IV-11.cs
@@ -0,0 +1,42 @@
+/*Для каждой строки найти последний четный элемент и записать данные в новый массив.*/
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк и столбцов: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] A = new int[n, n];
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                A[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int[] B = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int even = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (A[i, j] % 2 == 0)
+                {
+                    even = A[i, j];
+                }
+            }
+            B[i] = even;
+        }
+
+        Console.WriteLine("Последние четные элементы в каждой строке:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(B[i]);
+        }
+    }
+}
