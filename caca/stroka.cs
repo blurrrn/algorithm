42a7c41fbb6a31758e8d8ad6df8895046f81a25a
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-10-06 11:15:40 +0400

    Update stroka.cs

--- caca/stroka.cs
+++ caca/stroka.cs
@@ -1,18 +1,25 @@
+using System.Collections.Generic;
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string message = "Дана строка, в которой содержится осмысленное текстовое сообщение";
+        string message = Console.ReadLine();
         char[] separators = { ' ', ',', '.', '!', '?' };
         string[] words = message.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        List<string> Max = new List<string>();
         string longestWord = "";
         foreach (string word in words)
         {
             if (word.Length > longestWord.Length)
             {
                 longestWord = word;
+                Max.Clear();
+            }
+            else if (word.Length == longestWord.Length)
+            {
+                Max.Add(word);
             }
         }
         Console.WriteLine("Самое длинное слово: " + longestWord);
