75579f39d033f48b43514af3f24866048dadec0b
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-11-16 23:56:08 +0400

    Create pract14-II-1.cs

---
+++ pract14-II-1.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IO;
+using System.Text;
+class Structures
+{
+// Создаем структуру Student
+public struct Student : IComparable<Student>
+{
+    // Поля структуры
+    public string LastName;
+    public string FirstName;
+    public string MiddleName;
+    public int BirthYear;
+    public string Address;
+    public string School;
+        // Конструктор, принимающий входные данные через запятую
+        public Student(string lastName, string firstName, string middleName, int birthYear, string address, string school)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+            BirthYear = birthYear;
+            Address = address;
+            School = school;
+        }
+
+        // Конструктор, принимающий одну строковую переменную line
+        public Student(string line)
+        {
+            string[] data = line.Split(' ');
+            LastName = data[0];
+            FirstName = data[1];
+            MiddleName = data[2];
+            BirthYear = int.Parse(data[3]);
+            Address = data[4];
+            School = data[5];
+        }
+        public string GetStudentData()
+        {
+            return $"{this.LastName} {this.FirstName} {this.MiddleName} {this.BirthYear} {this.Address} {this.School}";
+        }
+        // Метод для сравнения студентов на основе года рождения
+        public int CompareTo(Student other)
+    {
+        return this.BirthYear.CompareTo(other.BirthYear);
+    }
+static void Main()
+    {
+        string input_path = "C:\\education\\reshetki_kudrina\\test_prj\\pr14\\input1.txt";
+        string output_path = "C:\\education\\reshetki_kudrina\\test_prj\\pr14\\output1.txt";
+        using (StreamReader reader = new StreamReader(input_path))
+        {
+            using (StreamWriter writer = new StreamWriter(output_path, false))
+            {
+                List<Student> StudentsList = new List<Student>();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    StudentsList.Add(new Student(line));
+                }
+                StudentsList.Sort();
+                foreach (Student student in StudentsList)
+                    {
+                        writer.WriteLine(student.GetStudentData());
+                    }
+            }
+        }
+    }
+}
