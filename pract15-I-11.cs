63dfda246ec5903bdc1124fc37b983b9bcb6e4ca
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-11-17 10:53:42 +0400

    Create pract15-I-11.cs

---
+++ pract15-I-11.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace chisla {
+    class Program {
+        static void Main(string[] args) {
+            string pathInput = "numbers.txt";
+            List<int> Numbers = new List<int>();
+            string FileContent = File.ReadAllText(pathInput);
+            string[] string_numbers = FileContent.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string number in string_numbers)
+            {
+                Numbers.Add(int.Parse(number));
+            }
+            var SortedNumbers = from n in Numbers
+                                where n >= 10 && n <= 99
+                                select -n;
+            string path_output = "output.txt";
+            using (StreamWriter fout = new StreamWriter(path_output, false))
+            {
+                foreach (var n in SortedNumbers)
+                    fout.Write($"{n} ");
+            }
+        }
+    }
+}
