19d471bd234893a2be09dfeaa0f16ba7e2c188db
blurrrn <103361796+blurrrn@users.noreply.github.com>
2023-11-24 00:12:11 +0400

    Update pract15-II-11.cs

--- pract15-II-11.cs
+++ pract15-II-11.cs
@@ -1,3 +1,7 @@
+//На основе данных входного файла составить инвентарную ведомость
+//склада, включив следующие данные: вид продукции, стоимость, сорт, количество. Вывести в
+//новый файл информацию о той продукции, количество которой менее заданной величины,
+//отсортировав ее по количеству продукции на складе.
 using System;
 using System.IO;
 using System.Text;
@@ -9,11 +13,11 @@ class Structures
     {
         // Поля структуры
         public string ProdType;
-        public string Price;
+        public int Price;
         public string Variety;
         public int Amount;
         // Конструктор, принимающий входные данные через запятую
-        public Tovar(string prodType, string price, string variety, int amount, string address, string school)
+        public Tovar(string prodType, int price, string variety, int amount)
         {
             ProdType = prodType;
             Price = price;
@@ -26,7 +30,7 @@ class Structures
         {
             string[] data = line.Split(' ');
             ProdType = data[0];
-            Price = data[1];
+            Price = int.Parse(data[1]);
             Variety = data[2];
             Amount = int.Parse(data[3]);
         }
@@ -38,27 +42,31 @@ class Structures
         {
             string input_path = "input.txt";
             string output_path = "output.txt";
+            //Console.WriteLine("Введите заданное количество: ");
+            //Con
             using (StreamReader reader = new StreamReader(input_path))
             {
                 using (StreamWriter writer = new StreamWriter(output_path, false))
                 {
                     List<Tovar> TovarList = new List<Tovar>();
                     string line;
-                    Console.Write("Введите Amount: ");
+                    Console.Write("Введите заданное количество: ");
                     int k = int.Parse(Console.ReadLine());
                     while ((line = reader.ReadLine()) != null)
                     {
                         TovarList.Add(new Tovar(line));
                     }
-                    //var sortedTov =
-                    //    from Amount in TovarList
-                    //    orderby Amount < k
-                    //    select n;
-                    foreach (Tovar student in TovarList)
-                    {
-                        if (student.School == school)
-                            writer.WriteLine(student.GetTovarData());
-                    }
+                    var sortedTov1 =
+                        from t in TovarList
+                        where t.Amount < k
+                        select t;
+                    var sortedTov2 =
+                        from t in sortedTov1
+                        orderby t.Amount
+                        select t;
+                    foreach (var t in sortedTov2)
+                        writer.WriteLine(t.GetTovarData());
+
                 }
             }
         }
